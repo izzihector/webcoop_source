9058e957b8e179a514245e8480f089003bf6b790
Daisuke Suzuki <suzukid@e-supportlink.co.jp>
2019-08-09 09:36:06 +0900

    Feature #530

--- wc_usembassy/member.py
+++ wc_usembassy/member.py
@@ -15,7 +15,33 @@ class Member(models.Model):
     division = fields.Char('Agency/Division',track_visibility='onchange')
     date_of_employement = fields.Date('USG Date of Employment',track_visibility='onchange')
     payroll_number = fields.Char('Payroll Number',track_visibility='onchange')
+
+    employee_code = fields.Char('Employee ID',track_visibility='onchange',old_name='employee_id')
+    member_age = fields.Float(compute="_get_member_age", digits=(12,2))
+    fund_transfer_type = fields.Selection([('citi','CITI Bank'),('pnb','PNG'),
+                                           ('check','Check Account Number'),
+                                           ('coop','Cooperative Savings')],string="Fund Transfer")
+    fund_transfer = fields.Char()
     
+    @api.depends('membership_date','membership_end_date')
+    def _get_member_age(self):
+        today = fields.Date.from_string(fields.Date.context_today(self))
+        days_in_year = 365.2425
+        for r in self:
+            if r.membership_end_date:
+                    tdate = fields.Date.from_string(r.membership_end_date)
+            else:
+               tdate = today
+            if r.membership_date:
+                r.member_age = (tdate - fields.Date.from_string(r.membership_date)).days / days_in_year
+            else:
+                r.member_age =False
+ 
+    @api.onchange('fund_transfer_type')
+    def onchange_fund_type(self):
+        for r in self:
+            if not r.fund_transfer_type:
+                return {'value':{'fund_transfer':False}}
 
 
 
