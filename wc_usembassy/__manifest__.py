206981bb66a373044722a9b318d0117976e8105c
Daisuke Suzuki <suzukid@e-supportlink.co.jp>
2019-11-04 03:46:37 +0900

    manifest file of usemb is incorrect

--- wc_usembassy/__manifest__.py
+++ wc_usembassy/__manifest__.py
@@ -7,11 +7,10 @@
     'description': """
                     1.Allow Bulk import and approve for account transaction,
                     2.Add custom field on membership form,
-                    3.Allow Bulk import for payment (actual logic is in wc_loan_payment_bulkimport)
                    """,
     'author': 'Esupportlink.',
     'depends': [
-        'wc_account','wc','wc_posting','wc_member','wc_loan_payment_bulkimport'
+        'wc_account','wc','wc_posting','wc_member'
     ],
     'init_xml': [],
     'data': [
