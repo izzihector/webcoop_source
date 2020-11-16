d436880b3c515143a7f952c482d23b305ed52c63
Raeceph Jude Sayson <rsayson@csyoutsourcing.com>
2020-11-16 15:44:50 +0800

    for testing

--- wc_usembassy/__manifest__.py
+++ wc_usembassy/__manifest__.py
@@ -13,6 +13,7 @@
                     6.loan gurantee fund type saving account
                     7.consolidate rebate feature
                     8.loan disbursement feature
+                    9. This is a test
                    """,
     'author': 'Esupportlink',
     'depends': [
